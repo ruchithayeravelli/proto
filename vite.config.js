9781b6a975fbb15f3b3df58476a5d61467201321
Ruchitha Yeravelli <yeravelliruchitha@gmail.com>
2026-02-23 18:05:01 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/proto/',   // ✅ IMPORTANT — must match repo name
+  base: '/proto/',
 })
